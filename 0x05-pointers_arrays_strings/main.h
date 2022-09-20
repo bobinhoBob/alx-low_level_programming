098e01fe779e012dc6fd8bf0010b435ade0eecee
bobinhoBob <mishack.rakgalakane1@gmail.com>
2022-09-19 17:41:04 -0700

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,8 @@
 * int prototypes()
 */
 
-void reset_to_98(int *n)
-void swap_int(int *a, int *b)
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -16,4 +16,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(char *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+
 #endif /*MAIN_H*/
