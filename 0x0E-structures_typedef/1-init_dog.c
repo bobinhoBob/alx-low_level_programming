3350f8d95cd29f8922241da45cad0a9e18a4e2c1
bobinhoBob <mishack.rakgalakane1@gmail.com>
2022-10-11 12:45:19 -0700

    Structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,13 +9,14 @@
  * @owner: The owner of the dog.
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+	else
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
 }
-
